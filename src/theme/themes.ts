b4f9a2b6c999f9c6a9e75c65aa3593e790d17875
AnanthuTD <ananthutd2021@gmail.com>
2024-08-15 17:35:42 +0530

    feat: Add theme configurations with shared options and deepmerge support

---
+++ src/theme/themes.ts
@@ -0,0 +1,68 @@
+import { ThemeOptions } from '@mui/material/styles';
+import { deepmerge } from '@mui/utils';
+
+import { Themes } from './types';
+
+const sharedTheme = {
+  palette: {
+    background: {
+      default: '#fafafa',
+      paper: '#fff',
+    },
+    slateBlue: {
+      main: '#505ABB',
+      contrastText: '#ffffff',
+    },
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiDivider: {
+      styleOverrides: {
+        vertical: {
+          marginRight: 10,
+          marginLeft: 10,
+        },
+        middle: {
+          marginTop: 10,
+          marginBottom: 10,
+          width: '80%',
+        },
+      },
+    },
+  },
+} as ThemeOptions; // the reason for this casting is deepmerge return type
+// TODO (Suren): replace mui-utils-deepmerge with lodash or ramda deepmerge
+
+const themes: Record<Themes, ThemeOptions> = {
+  light: deepmerge(sharedTheme, {
+    palette: {
+      mode: 'light',
+      background: {
+        default: '#fafafa',
+        paper: '#fff',
+      },
+      primary: {
+        main: '#3f51b5',
+      },
+    },
+  }),
+
+  dark: deepmerge(sharedTheme, {
+    palette: {
+      mode: 'dark',
+      background: {
+        default: '#111',
+        paper: '#171717',
+      },
+      primary: {
+        main: '#333',
+      },
+    },
+  }),
+};
+
+export default themes;
