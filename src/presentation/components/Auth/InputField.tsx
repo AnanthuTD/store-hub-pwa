5cfe383b375b945bc02e8113be15c049dfedfb1b
AnanthuTD <ananthutd2021@gmail.com>
2024-08-15 17:56:25 +0530

    feat: Add SignUpInputField component for user input

---
+++ src/presentation/components/Auth/InputField.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TextField } from '@mui/material';
+
+interface SignUpInputFieldProps {
+  label: string;
+  value: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+  disabled?: boolean;
+}
+
+const SignUpInputField: React.FC<SignUpInputFieldProps> = ({
+  label,
+  value,
+  onChange,
+  type = 'text',
+  disabled = false,
+}) => {
+  return (
+    <TextField
+      label={label}
+      variant="outlined"
+      fullWidth
+      margin="none"
+      size="small"
+      value={value}
+      onChange={onChange}
+      type={type}
+      disabled={disabled}
+    />
+  );
+};
+
+export default SignUpInputField;
