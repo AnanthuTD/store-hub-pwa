469a15223d6bf4eefa735abc86464971b4d81386
AnanthuTD <ananthutd2021@gmail.com>
2024-08-15 17:29:59 +0530

    feat: Initialize main App component with routing and error handling

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { withErrorHandler } from '@/error-handling';
+import AppErrorBoundaryFallback from '@/error-handling/fallbacks/App';
+import Pages from '@/presentation/routes/Pages';
+// import Notifications from '@/sections/Notifications';
+import SW from '@/presentation/sections/SW';
+
+function App() {
+  return (
+    <Fragment>
+      <CssBaseline />
+      {/* <Notifications /> */}
+      <SW />
+      <BrowserRouter>
+        <Pages />
+      </BrowserRouter>
+    </Fragment>
+  );
+}
+
+export default withErrorHandler(App, AppErrorBoundaryFallback);
