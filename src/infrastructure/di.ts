7507ee913c2e7771b2412110b2aa11a673aeaef3
AnanthuTD <ananthutd2021@gmail.com>
2024-08-15 17:51:55 +0530

    chore: Set up and configure dependency injection for use cases and repositories
    - Configured and instantiated dependencies for use cases related to authentication and country codes.
    
      - **Repositories**:
        - `AuthRepositoryImpl`: Provides implementation for authentication-related API interactions.
        - `CountryCodeRepositoryImpl`: Fetches country code data from the API.
    
      - **Use Cases**:
        - `FetchCountryCodesUseCase`: Set up with `CountryCodeRepositoryImpl` to handle country code retrieval.
        - `VerifyEmailUseCase`: Configured with `AuthRepositoryImpl` to manage email verification operations.
    
    - Exported `fetchCountryCodesUseCase` and `verifyEmailUseCase` instances for integration into other modules.
    
      - This setup centralizes dependency management, allowing for easier testing and maintenance by providing well-defined entry points for service interactions.

---
+++ src/infrastructure/di.ts
@@ -0,0 +1,13 @@
+import { CountryCodeRepositoryImpl } from '@/infrastructure/repositories/CountryCodeRepositoryImpl';
+import { FetchCountryCodesUseCase } from '@/application/usecase/FetchCountryCodesUseCase';
+import { AuthRepositoryImpl } from '@/infrastructure/repositories/AuthRepositoryImpl';
+import { VerifyEmailUseCase } from '@/application/usecase/VerifyEmailUseCase';
+
+const authRepository = new AuthRepositoryImpl();
+
+const countryCodeRepository = new CountryCodeRepositoryImpl();
+
+const fetchCountryCodesUseCase = new FetchCountryCodesUseCase(countryCodeRepository);
+const verifyEmailUseCase = new VerifyEmailUseCase(authRepository);
+
+export { fetchCountryCodesUseCase, verifyEmailUseCase };
