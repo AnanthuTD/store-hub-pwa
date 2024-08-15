d32c67aa83696a482a0665eb376a22d9cc22a993
AnanthuTD <ananthutd2021@gmail.com>
2024-08-15 17:56:08 +0530

    feat: Add GoogleSignUpButton component for Google authentication

---
+++ src/presentation/components/Auth/GoogleSignUpButton.tsx
@@ -0,0 +1,29 @@
+import { Button, Link } from '@mui/material';
+
+const GoogleSignUpButton = () => {
+  return (
+    <Link href={`${import.meta.env.VITE_API_BASE_URL}/auth/google`}>
+      <Button
+        variant="outlined"
+        startIcon={<img src={'/google-logo.svg'} />}
+        sx={{
+          backgroundColor: '505ABB',
+          borderRadius: '50px',
+          padding: '10px 20px',
+          borderColor: 'rgba(0, 0, 0, 0.2)',
+          textTransform: 'none',
+          fontWeight: 'normal',
+          color: 'black',
+          '&:hover': {
+            borderColor: 'rgba(0, 0, 0, 0.4)',
+          },
+        }}
+        fullWidth
+      >
+        Sign up with your Google account
+      </Button>
+    </Link>
+  );
+};
+
+export default GoogleSignUpButton;
