4e3e2dfe549d8011111f95c3d45e8e89a4d350bb
AnanthuTD <ananthutd2021@gmail.com>
2024-08-16 00:00:17 +0530

    feat: Define route types and page enum for improved routing configuration
    - Added `Pages` enum to uniquely identify different routes in the application.
    - Created `PathRouteCustomProps` type to include optional title and icon for each route.
    - Defined `Routes` type as a mapping of `Pages` to route configuration including custom properties.
    - Added comments for better understanding and maintainability of route configuration.
    
    This setup provides a structured and type-safe way to manage routing in the application, allowing for easier updates and scalability.

---
+++ src/presentation/routes/types.ts
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { PathRouteProps } from 'react-router-dom';
+
+import type { SvgIconProps } from '@mui/material/SvgIcon';
+
+enum Pages {
+  SignUp,
+  SignUpWithEmail,
+  SignUpWithMobile,
+  SignIn,
+  UserHome,
+}
+
+type PathRouteCustomProps = {
+  title?: string;
+  component: FC;
+  icon?: FC<SvgIconProps>;
+};
+
+type Routes = Record<Pages, PathRouteProps & PathRouteCustomProps>;
+
+export type { Routes };
+export { Pages };
