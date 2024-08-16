a18df2e1eb52690c03bd9901f8dfaecb9ba22075
AnanthuTD <ananthutd2021@gmail.com>
2024-08-16 10:36:23 +0530

    feat: Add Redux store integration with redux for state management

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import AppErrorBoundaryFallback from '@/error-handling/fallbacks/App';
 import Pages from '@/presentation/routes/Pages';
 // import Notifications from '@/sections/Notifications';
 import SW from '@/presentation/sections/SW';
+import { store } from './infrastructure/redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
@@ -15,9 +17,11 @@ function App() {
       <CssBaseline />
       {/* <Notifications /> */}
       <SW />
-      <BrowserRouter>
-        <Pages />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Pages />
+        </BrowserRouter>
+      </Provider>
     </Fragment>
   );
 }
