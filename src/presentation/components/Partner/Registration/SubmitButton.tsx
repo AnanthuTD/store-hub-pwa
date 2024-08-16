91229a4024ef86c9e6e7d07bfef5812c164a9785
AnanthuTD <ananthutd2021@gmail.com>
2024-08-16 19:51:53 +0530

    feat: Added props `disable` to  disable button.

--- src/presentation/components/Partner/Registration/SubmitButton.tsx
+++ src/presentation/components/Partner/Registration/SubmitButton.tsx
@@ -4,9 +4,10 @@ import { Button } from '@mui/material';
 interface SubmitButtonProps {
   text: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-const SubmitButton: React.FC<SubmitButtonProps> = ({ text, onClick }) => {
+const SubmitButton: React.FC<SubmitButtonProps> = ({ text, onClick, disabled = false }) => {
   return (
     <Button
       variant="contained"
@@ -14,6 +15,7 @@ const SubmitButton: React.FC<SubmitButtonProps> = ({ text, onClick }) => {
       sx={{ backgroundColor: '#FF6464', color: '#FFF', borderRadius: 4 }}
       onClick={onClick}
       size="large"
+      disabled={disabled}
     >
       {text}
     </Button>
