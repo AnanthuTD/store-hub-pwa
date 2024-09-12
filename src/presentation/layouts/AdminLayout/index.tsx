ecc73a0236a8f443a7cd8699c2d8eff77ce464b7
AnanthuTD <ananthutd2021@gmail.com>
2024-09-12 06:05:14 +0530

    feat: admin-au

--- src/presentation/layouts/AdminLayout/index.tsx
+++ src/presentation/layouts/AdminLayout/index.tsx
@@ -1,13 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ConfigProvider, Layout, theme } from 'antd';
 import Sidebar from './components/SideBar';
 import HeaderBar from './components/HeaderBar';
 import ContentArea from './components/ContentArea';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { AppDispatch, RootState } from '@/infrastructure/redux/store';
+import { fetchProfile } from '@/infrastructure/repositories/AdminRepository';
+import Cookies from 'js-cookie';
+import { login } from '@/infrastructure/redux/slices/admin/adminSlice';
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const [collapsed, setCollapsed] = useState(false);
 
   const toggleCollapsed = () => setCollapsed(!collapsed);
+
+  const admin = useSelector((state: RootState) => state.admin.data);
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const loadProfile = async () => {
+      try {
+        const tokenFromUrl = searchParams.get('token');
+
+        // If a token is found in the URL, store it in the cookies
+        if (tokenFromUrl) {
+          Cookies.remove('authToken');
+          Cookies.set('authToken', tokenFromUrl);
+        }
+
+        // If no shopOwner data, attempt to fetch the profile
+        if (!admin) {
+          const profile = await fetchProfile();
+          if (!profile) {
+            navigate('/admin/signin');
+          } else {
+            // Update Redux store with fetched profile data
+            console.log(profile);
+
+            dispatch(login(profile));
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching profile:', error);
+        navigate('/admin/signin'); // Navigate to sign-in if fetching profile fails
+      }
+    };
+
+    loadProfile();
+  }, [admin, dispatch, navigate, searchParams]);
+
+  // TODO: Uncomment the following line to restrict access to specific routes based on admin role
+  /*   if (!admin) {
+    return null;
+  } */
 
   return (
     <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}>
