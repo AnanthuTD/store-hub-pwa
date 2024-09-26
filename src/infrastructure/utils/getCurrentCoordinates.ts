26f65d879d2a4fd699b1218e54b4306d45695c63
AnanthuTD <ananthutd2021@gmail.com>
2024-09-26 21:19:49 +0530

    feat: util to get the current location

---
+++ src/infrastructure/utils/getCurrentCoordinates.ts
@@ -0,0 +1,29 @@
+export default async function getCurrentCoordinates() {
+  if (!navigator.geolocation) {
+    throw new Error('Geolocation is not supported by your browser');
+  }
+
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        resolve({ latitude, longitude });
+      },
+      (error) => {
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            reject(new Error('Location access denied by user.'));
+            break;
+          case error.POSITION_UNAVAILABLE:
+            reject(new Error('Location information is unavailable.'));
+            break;
+          case error.TIMEOUT:
+            reject(new Error('Location request timed out.'));
+            break;
+          default:
+            reject(new Error('An unknown error occurred.'));
+        }
+      },
+    );
+  });
+}
