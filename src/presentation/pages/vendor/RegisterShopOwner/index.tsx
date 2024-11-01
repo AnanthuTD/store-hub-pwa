36760cc3fe3582cb240dad6d6189fb298e6aa935
AnanthuTD <ananthutd2021@gmail.com>
2024-11-01 07:34:58 +0530

    chore: removed unnecessary comments and console.logs

--- src/presentation/pages/vendor/RegisterShopOwner/index.tsx
+++ src/presentation/pages/vendor/RegisterShopOwner/index.tsx
@@ -12,52 +12,24 @@ const { Step } = Steps;
 
 type ShopOwner = Omit<IVendor, '_id'>;
 
-// const shopOwnerData = {
-//   email: 'shopOwner@gmail.com',
-//   bankDetails: {
-//     accountHolderName: 'John Doe',
-//     accountNumber: '1234567890',
-//     bankName: 'Sample Bank',
-//     ifscCode: 'SBIN0001234',
-//   },
-//   createdAt: new Date('2023-01-01T00:00:00Z'), // Assume a past date for creation
-//   phone: '+911234567890',
-//   updatedAt: new Date('2024-09-01T00:00:00Z'), // Assume the last update was recent
-//   profile: {
-//     address: {
-//       city: 'Mumbai',
-//       country: 'India',
-//       postalCode: '400001',
-//       state: 'Maharashtra',
-//       street: '123, Sample Street',
-//     },
-//     firstName: 'John',
-//     lastName: 'Doe',
-//   },
-// };
-
 function Index() {
   const shopOwner = useSelector((state: RootState) => state.vendor.data);
 
-  const [step, setStep] = useState(0); // Step 1 for form, Step 2 for file upload
+  const [step, setStep] = useState(0);
 
   const handleUploadComplete = () => {
-    // Optionally handle post-upload actions here
     setStep(3);
     notification.success({ message: 'Success', description: 'Registration complete!' });
   };
 
   async function handleFormSubmit(shopOwner: ShopOwner) {
     try {
-      const response = await axiosInstance.post('/vendor/register', shopOwner); // Await the response from axios
-      console.log(response);
-      setStep(1); // Assuming setStep is defined in your component or context
+      await axiosInstance.post('/vendor/register', shopOwner);
+      setStep(1);
     } catch (err) {
-      // Ensure to check if err is an AxiosError and handle accordingly
       if (err instanceof AxiosError && err.response) {
         notification.error({ message: 'Error', description: err.response.data.message });
       } else {
-        // Handle unexpected errors
         notification.error({ message: 'Error', description: 'An unexpected error occurred' });
       }
     }
