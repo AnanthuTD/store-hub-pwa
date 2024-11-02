126551177faeed7e56940b14309cd53807c048d1
AnanthuTD <ananthutd2021@gmail.com>
2024-11-02 20:00:36 +0530

    feat: updated carousel to use fetched data.

--- src/presentation/pages/user/components/Carousel.tsx
+++ src/presentation/pages/user/components/Carousel.tsx
@@ -3,25 +3,46 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper/modules';
 import { Box, Button, Typography } from '@mui/material';
 import SwiperCore from 'swiper';
+import { useEffect, useState } from 'react';
+import axiosInstance from '@/config/axios';
+import { message } from 'antd';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+interface Banner {
+  _id: string;
+  title: string;
+  subtitle?: string;
+  imageUrl?: string;
+  link?: string;
+  startDate: string;
+  endDate: string;
+  buttonText: string;
+}
 
 SwiperCore.use([Autoplay, Pagination]);
 
-const carouselData = [
-  {
-    title: 'GROCERIES DELIVERY',
-    description: 'Find the Nearest Stores with Your Desired Products',
-    buttonText: 'Start Now',
-    image: '/carousel.png',
-  },
-  {
-    title: 'FRESH PRODUCTS',
-    description: 'Get Fresh Products Delivered at Your Doorstep',
-    buttonText: 'Order Now',
-    image: '/carousel.png',
-  },
-];
-
 const Carousel = () => {
+  const [banners, setBanners] = useState<Banner[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchBanners();
+  }, []);
+
+  const fetchBanners = async () => {
+    try {
+      const response = await axiosInstance.get<Banner[]>('/user/banners');
+      setBanners(response.data);
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        message.error(
+          e.response?.data?.message || 'Failed to fetch banner! Please try again after some time.',
+        );
+      } else message.error('Failed to fetch banners');
+    }
+  };
+
   return (
     <Swiper
       modules={[Autoplay, Pagination]}
@@ -30,12 +51,12 @@ const Carousel = () => {
       autoplay={{ delay: 3000, disableOnInteraction: false }}
       loop={true}
     >
-      {carouselData.map((item, index) => (
+      {banners.map((item, index) => (
         <SwiperSlide key={index}>
           <Box
             sx={{
               height: '500px',
-              background: `url(${item.image}) center center/cover no-repeat`,
+              background: `url(${item.imageUrl}) center center/cover no-repeat`,
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
@@ -49,7 +70,7 @@ const Carousel = () => {
               {item.title}
             </Typography>
             <Typography variant="body1" sx={{ margin: '20px 0' }}>
-              {item.description}
+              {item.subtitle}
             </Typography>
             <Button
               variant="contained"
@@ -59,6 +80,7 @@ const Carousel = () => {
                 borderRadius: '20px',
                 padding: '10px 20px',
               }}
+              onClick={() => navigate(item.link)}
             >
               {item.buttonText}
             </Button>
